83770bae6379f951ab1a5e6d42c97f4cb552a2bc
sunkun <87983133@qq.com>
2020-07-23 19:39:14 +0800

    链表初步

---
+++ src/com/mashibing/leetcode/ReverseList.java
@@ -0,0 +1,13 @@
+package com.mashibing.leetcode;
+
+//206
+public class ReverseList {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+}
