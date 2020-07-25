791b8c4c1e6c6e653126d6d9c3d524954ae73aac
sunkun <87983133@qq.com>
2020-07-25 16:51:35 +0800

    动态规划

--- src/com/mashibing/leetcode/DynamicProgramming/LongestCommonSubsequence.java
+++ src/com/mashibing/leetcode/DynamicProgramming/LongestCommonSubsequence.java
@@ -1,8 +1,52 @@
 package com.mashibing.leetcode.DynamicProgramming;
 
+/***  1143
+ * 对于两个子序列 S1 和 S2，找出它们最长的公共子序列。
+ *
+ * 定义一个二维数组 dp 用来存储最长公共子序列的长度，
+ * 其中 dp[i][j] 表示 S1 的前 i 个字符与 S2 的前 j 个字符最长公共子序列的长度。考虑 S1i 与 S2j 值是否相等，分为两种情况：
+ *
+ * 当 S1i==S2j 时，那么就能在 S1 的前 i-1 个字符与 S2 的前 j-1 个字符最长公共子序列的基础上再加上 S1i 这个值，
+ * 最长公共子序列长度加 1，即 dp[i][j] = dp[i-1][j-1] + 1。
+ * 当 S1i != S2j 时，此时最长公共子序列为 S1 的前 i-1 个字符和 S2 的前 j 个字符最长公共子序列，
+ * 或者 S1 的前 i 个字符和 S2 的前 j-1 个字符最长公共子序列，取它们的最大者，即 dp[i][j] = max{ dp[i-1][j], dp[i][j-1] }。
+ */
 public class LongestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if (text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return  dp[m][n];
+    }
 
-        return 0;
+
+    public static void main(String[] args) {
+        String text1="a";
+        String text2="a";
+        LongestCommonSubsequence longestCommonSubsequen= new LongestCommonSubsequence();
+        System.out.println(longestCommonSubsequen.longestCommonSubsequence(text1,text2));
+    }
+
+    public int longestCommonSubsequence2(String str1, String str2) {
+        if (str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0)
+            return 0;
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[str1.length()][str2.length()];
     }
 }
